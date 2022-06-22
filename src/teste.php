ca970e25a94a62eea0cd9b094ffe76c2897249f4
jp9910 <jops40@hotmail.com>
2022-06-22 17:50:50 -0300

    TL:Avaliação - Sindicado dos Trainees - Teste de conexão com banco de dados
    Author: Joao Paulo Secundo <joaopaulo@moobitech.com.br>
    Date:      Wed Jun 22 17:50:50 2022 -0300

--- src/teste.php
+++ src/teste.php
@@ -1,7 +1,16 @@
 <?php
 
 namespace Moobi\Avaliacao;
-require '/var/www/html/src/Core/Router.php';
 use Moobi\Avaliacao\Core\Router;
+use PDO;
 
-$router = new Router;
+$connection = new PDO('mysql:host=db;dbname=sindicato_trainees','root','password');
+echo 'Conectado' . PHP_EOL;
+
+$sql = 'SELECT * FROM sindicato_trainees.tse_teste';
+
+$statement = $connection->query($sql);
+
+$resultado = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($resultado);
