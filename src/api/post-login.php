5a0ba6ae1c157d896411a06c212f0dca95dad0ed
jp9910 <jops40@hotmail.com>
2022-06-29 18:06:43 -0300

    API de login

--- src/api/post-login.php
+++ src/api/post-login.php
@@ -1,12 +1,29 @@
 <?php 
 
+/**
+ * API POST - Faz a checagem da informação de login e retorna o status em json.
+ */
+
 namespace Jp\SindicatoTrainees\api;
 
 use Jp\SindicatoTrainees\domain\controllers\UsuarioController;
 
-header('Content-Type: application/json');
+//header('Content-Type: application/json');
 
 $oController = new UsuarioController();
 $json = $oController->login($_REQUEST['usuario'], $_REQUEST['senha']);
 
-echo $json;
+$status = json_decode($json);
+if ($status->status === 200) {
+    $_SESSION = array(); // Unset all of the session variables
+    session_destroy();
+    session_start();
+    $_SESSION['usuario_id'] = $status->usuario->id;
+    $_SESSION['usuario_login'] = $status->usuario->login;
+    $_SESSION['usuario_nome'] = $status->usuario->nome;
+    $_SESSION['logado'] = true;
+    //header("Location: /home");
+    //die();
+}
+var_dump($_SESSION);
+//echo $json;
