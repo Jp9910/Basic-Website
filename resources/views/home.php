77bb961a419ff86bc2a21df98d0e8a0311a056ab
jp9910 <jops40@hotmail.com>
2022-07-04 15:31:28 -0300

    Adicionar logo

--- resources/views/home.php
+++ resources/views/home.php
@@ -8,15 +8,19 @@
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link type="text/css" rel="stylesheet" href="resources/libs/materialize/css/materialize.css"  media="screen,projection"/>
 	<link rel="stylesheet" type="text/css" href="resources/css/estilo.css">
+	<link rel="icon" href="resources/img/indice.png">
 </head>
 <body class="teal accent-2">
 	<nav>
 		<div class="nav-wrapper">
-			<a href="#" class="brand-logo right">Logo</a>
-			<ul id="nav-mobile" class="left hide-on-med-and-down">
+			<a href="/" class="brand-logo left">
+				<!-- <span src="/resources/img/sample-logo.png" ></span> -->
+				<img src="/resources/img/sample-logo.png" alt="logo" id="img-logo">
+			</a>
+			<ul id="nav-mobile" class="right hide-on-med-and-down">
 				<li><a href="listar-usuarios">Usuários do Sistema</a></li>
-				<li><a href="badges.html">Components</a></li>
-				<li><a href="collapsible.html">JavaScript</a></li>
+				<li><a href="badges.html">Empresas</a></li>
+				<li><a href="collapsible.html">Filiados</a></li>
 			</ul>
 		</div>
 	</nav>
