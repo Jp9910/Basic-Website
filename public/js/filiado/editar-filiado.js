445e2bd1174b7c0990d33df227ea78d8bcd3d92e
Joao Secundo <alundra@academico.ufs.br>
2022-07-14 21:42:50 -0300

    Edição de filiados - Select de empresa, cargo e situação

--- public/js/filiado/editar-filiado.js
+++ public/js/filiado/editar-filiado.js
@@ -5,6 +5,10 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
 });
 
 $(document).ready(function () {
+	$('#spinner').show();
+	buscarEmpresasAjax();
+	buscarCargosAjax();
+	buscarSituacaosAjax();
 	getFiliadoAjax();
 	$('form').attr('action', '#');
 	adicionarFuncaoBotaoExcluir();
@@ -68,14 +72,59 @@ function getFiliadoAjax()
 
 function preencherInfoFiliado(data, textStatus, jqXHR)
 {
-	$('#input-nome').val(data.nome)
-	$('#input-CPF').val(data.CPF)
-	$('#input-RG').val(data.RG)
-	$('#input-dataNascimento').val(data.dataNascimento.date.substr(0,10))
-	$('#input-idade').val(data.idade)
-	$('#input-telefone').val(data.telefone)
-	$('#input-celular').val(data.celular)
-	$('#input-empresa').val(data.empresa)
-	$('#input-cargo').val(data.cargo)
-	$('#input-situacao').val(data.situacao)
+	setTimeout(function(){
+		$('#input-nome').val(data.nome);
+		$('#input-CPF').val(data.CPF);
+		$('#input-RG').val(data.RG);
+		$('#input-dataNascimento').val(data.dataNascimento.date.substr(0,10));
+		$('#input-idade').val(data.idade);
+		$('#input-telefone').val(data.telefone);
+		$('#input-celular').val(data.celular);
+		$('#select-empresa').val(data.empresa);
+		$('#select-cargo').val(data.cargo);
+		$('#select-situacao').val(data.situacao);
+		$('#spinner').hide();
+	}, 500)
+}
+
+function buscarEmpresasAjax()
+{
+	$.get("/all-empresas", montarSelectEmpresas, "json");
+}
+
+function buscarCargosAjax()
+{
+	$.get("/all-cargos", montarSelectCargos, "json");
+}
+
+function buscarSituacaosAjax()
+{
+	$.get("/all-situacaos", montarSelectSituacaos, "json");
+}
+
+function montarSelectEmpresas(data)
+{
+	let select = $('#select-empresa');
+	data.forEach(element => {
+		let option = $('<option>').attr('value',element.id).text(element.nome);
+		select.append(option);
+	});
+}
+
+function montarSelectCargos(data)
+{
+	let select = $('#select-cargo');
+	data.forEach(element => {
+		let option = $('<option>').attr('value',element.id).text(element.nome);
+		select.append(option);
+	});
+}
+
+function montarSelectSituacaos(data)
+{
+	let select = $('#select-situacao');
+	data.forEach(element => {
+		let option = $('<option>').attr('value',element.id).text(element.nome);
+		select.append(option);
+	});
 }
